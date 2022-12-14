ec39d55cd6d4bf32deab2b6c2252f526f5cf5b28
Rocket <jyp@Rocket.local>
2022-12-14 19:35:16 +0900

    To the Moon

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+import Router from "./Router";
+
+function App() {
+  return <Router />;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./theme";
+
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+  <ThemeProvider theme={darkTheme}>
+    <App />
+  </ThemeProvider>
+);
